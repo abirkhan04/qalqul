b8a7729ad50cd59a5d578b24a1070c80bc2d9112
unknown <abirkhan8536@gmail.com>
2022-12-24 16:45:22 +0600

    [] Retrieving access token

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
+
+const { default: axios } = require('axios');
+
 const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json());
 const port = 3000;  
-
-
+const oauthUrl="https://graph.facebook.com/oauth/access_token?client_id=6097851830225464&client_secret=0c57fb1bf447e5c3f928237e72522469&grant_type=client_credentials";
+ 
   app.get('/test', function(req, res) {
      res.send({message: "This is test"});
   });
@@ -25,9 +28,10 @@ const port = 3000;
       console.log('Facebook request body:');
       console.log(JSON.stringify(req.body))
       console.log('Facebook request body end:'); 
+      axios.get(oauthUrl).then((response)=> {
+         console.log(JSON.stringify(response));
+      });
       res.sendStatus(200);
    });  
-
-
 
 app.listen(process.env.PORT || port, () => console.log(`webhook is listening on port ${port}`));
